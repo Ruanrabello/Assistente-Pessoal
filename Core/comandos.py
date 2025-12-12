65457fcd2c8c2263994721cd7c2f7ca3921ad95d
Ruan Rabello <ruanben7@gmail.com>
2025-12-12 00:14:37 -0300

    Atualização 1

---
+++ Core/comandos.py
@@ -0,0 +1,113 @@
+# ? Importa o modulu que permite que o python abra urls no navegador, como youtube etc
+import webbrowser
+import datetime  # ? módulo de data/hora
+import os  # ? módulo do sistema operacional
+# ? importa a função build da biblioteca googleapiclient. Esta função é crucial para conectar seu código à API de Dados do YouTube.
+from googleapiclient.discovery import build
+import pyautogui  # ? Importa biblioteca que simula comandos do teclado
+import time  # ? Importa biblioteca com funçoes de tempo
+
+YOUTUBE_API_KEY = "AIzaSyCPtKQkPYuKihl1RRmNsEe9HFfy-b7ZxMw"
+# ? Cria um objeto chamado youtube que será usado para fazer todas as chamadas à API, meio que o telefone para ligar para o servidor
+youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
+
+
+def abrir_youtube():
+    webbrowser.open("https://www.youtube.com/")
+    return "Abrindo YouTube."
+
+
+def Pausar_video():  # ? Função que pausa o video do youtube
+    pyautogui.press("k")
+    return "Video pausado."
+
+
+def fechar_aba_atual():  # ? Função que vai fechar a aba atual do youtube para abrir outra
+    pyautogui.hotkey("ctrl", "w")
+    return ""
+
+
+def abrir_google():
+    webbrowser.open("https://www.google.com.br/index.html")
+    return "Abrindo Google."
+
+
+def dizer_hora():  # ? pega a hora atual e retorna uma string formatada.
+    # ?  retorna um objeto datetime com data e hora atuais e converte esse objeto em uma str formatada, %H = hora com 2 dígitos, %M = minuto com 2 dígitos
+    agora = datetime.datetime.now().strftime("%H:%M")
+    return f"Agora são {agora}"
+
+
+# ? abre um programa no Windows pelo caminho completo.
+def abrir_programa(caminho):
+    try:
+        os.startfile(caminho)
+        return f"Abrindo programa em: {caminho}"
+    except Exception as e:
+        return f"Erro ao abrir o programa: {e}"
+
+
+def buscar_video(termo):  # ? Busca o primeiro vídeo correspondente ao termo.
+    req = youtube.search().list(  # ? constrói a requisição para a API
+        q=termo,  # ? termo de busca
+        # ! especifica que quer o trecho snippet dos resultados (título/descrição).
+        part="snippet",
+        maxResults=1,  # ? pede só 1 resultado
+        type="video"  # ? restringe a busca a vídeos (não playlists ou canais)
+    )
+
+    res = req.execute()
+
+    if not res["items"]:  # ? resposta da API tem uma estrutura JSON; se nada for encontrado, none em caso de erro e tals
+        return None
+
+    video_id = res["items"][0]["id"]["videoId"]
+    return f"https://www.youtube.com/watch?v={video_id}"
+
+
+def abrir_video(termo, fechar_anterior=True):
+    if fechar_anterior == True:
+        fechar_aba_atual()
+        time.sleep(0.5)
+    url = buscar_video(termo)  # ? chama a função para obter a url. 1- envia a requisição para a API do YouTube, 2- recebe a lista de resultados, 3- pega apenas o primeiro vídeo retornado, 4- extrai o videoId, 5- e monta a url(exempli: https://www.youtube.com/watch?v=VIDEO_ID_AQUI)
+
+    if not url:
+        return "Não encontrei esse vídeo. Verifique o nome ou sua API KEY."
+
+    webbrowser.open(url)
+    return f"Reproduzindo {termo} no YouTube."
+
+
+def interpretar_comando(texto):
+    texto = texto.lower()  # ? Deixa tudo em minusculo
+
+    # ? Palavras chaves para exexutar a função
+    comandos_musica = ["tocar", "ouvir", "reproduzir"]
+
+    for cmd in comandos_musica:  # ? Faz loop dentro da lista de palavras de ativação
+        if cmd in texto:  # ? Procura essa palavra no testo que vc falou
+            # ? ele pega o indice aonde esta a plavra de ativação
+            indice = texto.find(cmd)
+            # ? e pega o texto a frente(exemplo: tocar ride, ele pega ride para pesquisar)
+            termo_busca = texto[indice + len(cmd):].strip()
+
+            if termo_busca:
+                return abrir_video(termo_busca)
+            else:
+                return "Qual música você quer ouvir?"
+
+    # ----------------------------------
+    # 📌 Comandos básicos
+    # ----------------------------------
+    if "youtube" in texto:
+        return abrir_youtube()
+    elif "google" in texto:
+        return abrir_google()
+    elif "pausar" in texto or "despausar" in texto:
+        return Pausar_video()
+    elif "hora" in texto:
+        return dizer_hora()
+    elif "calculadora" in texto:
+        return abrir_programa("C:\\Windows\\System32\\calc.exe")
+
+    return None
